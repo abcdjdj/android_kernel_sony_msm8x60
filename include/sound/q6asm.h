4387271ec1c2da7a27d57e4785cb17a2e831d99f
RaymanFX <raymanfx@gmail.com>
2013-09-08 12:35:37 +0200

    q6asm: fix double declaration
    Change-Id: I5c331d9e9d01e5596e4d34e6e0f9be2dbba53b60

--- include/sound/q6asm.h
+++ include/sound/q6asm.h
@@ -187,9 +187,6 @@ int q6asm_audio_client_buf_free_contiguous(unsigned int dir,
 int q6asm_open_read(struct audio_client *ac, uint32_t format);
 int q6asm_open_read_v2_1(struct audio_client *ac, uint32_t format);
 
-int q6asm_open_read_compressed(struct audio_client *ac,
-			 uint32_t frames_per_buffer, uint32_t meta_data_mode);
-
 int q6asm_open_write(struct audio_client *ac, uint32_t format);
 
 int q6asm_open_write_compressed(struct audio_client *ac, uint32_t format);
